af791b20f25c495b11053257ff1e4edf781c6750
Hamilton Souza <61994118+hamiltonsouza@users.noreply.github.com>
2024-01-10 17:22:07 -0300

    Add exercise 03 solution

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,13 @@
-const reverseString = function() {
+const reverseString = function(originalString) {
+
+    let stringArray = originalString.split("");
+    let reversedArray = stringArray.reverse();
+    let reversedString = reversedArray.join("");
+    return reversedString;   
 
 };
+
+reverseString('hello');
 
 // Do not edit below this line
 module.exports = reverseString;
