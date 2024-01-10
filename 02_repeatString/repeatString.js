23dc440a1729b48598086582a8d01b6830fcc553
Hamilton Souza <61994118+hamiltonsouza@users.noreply.github.com>
2024-01-10 14:52:54 -0300

    Add exercise 02 solution to pass test 05

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,9 @@
 const repeatString = function(stringToRepeat,numberOfTimes) {
+    if (numberOfTimes >= 0) {
     return stringToRepeat.repeat(numberOfTimes);
+    } else {
+        return 'ERROR'
+    }
 };
 
 // Do not edit below this line
