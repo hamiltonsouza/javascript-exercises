75d67ec4d0aae0beaf01c8a364af81605824543e
Hamilton Souza <61994118+hamiltonsouza@users.noreply.github.com>
2024-01-11 15:22:36 -0300

    Add solution for exercise 05 test 03

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,11 +1,16 @@
 const sumAll = function(initialNumber,finalNumber) {
 
     let sum = 0;
-    
+
+    if (initialNumber > finalNumber) {
+        let numberPlaceholder = initialNumber;
+        initialNumber = finalNumber;
+        finalNumber = numberPlaceholder;
+    }
+
     for (let i = initialNumber; i <= finalNumber; i++) {
         sum = sum + i;
     }
-    
     return sum;
 
 };
