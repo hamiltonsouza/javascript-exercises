4caf44f906925ce0e117d089bdea46a71f8c3e56
Hamilton Souza <61994118+hamiltonsouza@users.noreply.github.com>
2024-01-11 18:16:54 -0300

    Add both conversion formulas

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,14 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fahrenheitTemperature) {
+
+  let celsiusTemperature = (fahrenheitTemperature - 32) * 5 / 9;
+  return celsiusTemperature;
+
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celsiusTemperature) {
+
+  let fahrenheitTemperature = (celsiusTemperature * 9 / 5) + 32;
+
 };
 
 // Do not edit below this line
