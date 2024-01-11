4379204c884b843e200c41063b8fc81de70c26d8
Hamilton Souza <61994118+hamiltonsouza@users.noreply.github.com>
2024-01-10 21:39:12 -0300

    Fixes solution to pass test 02

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,12 @@
-const removeFromArray = function(originalArray,indexToRemove) {
-    originalArray.splice(indexToRemove - 1, 1);
-    return originalArray;
+const removeFromArray = function(originalArray,firstIndexToRemove,secondIndexToRemove) {
+    if (secondIndexToRemove != false && secondIndexToRemove > -1) {
+        originalArray.splice(firstIndexToRemove - 1,1);
+        originalArray.splice(secondIndexToRemove - 1,1);
+        return originalArray;
+    } else {
+        originalArray.splice(firstIndexToRemove - 1,1);
+        return originalArray;
+    }
 };
 
 // Do not edit below this line
