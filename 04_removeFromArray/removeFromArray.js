11cdc58ae78b459955566be08b2aec58b7fe5fa3
Hamilton Souza <61994118+hamiltonsouza@users.noreply.github.com>
2024-01-10 21:36:59 -0300

    Add exercise 04 solution

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,6 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(originalArray,indexToRemove) {
+    originalArray.splice(indexToRemove - 1, 1);
+    return originalArray;
 };
 
 // Do not edit below this line
