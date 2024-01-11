eda2934d3e65163b3b975146691c4c674902c542
Hamilton Souza <61994118+hamiltonsouza@users.noreply.github.com>
2024-01-11 18:20:04 -0300

    Add rounding functionality and remaining tests

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,13 +1,16 @@
 const convertToCelsius = function(fahrenheitTemperature) {
 
   let celsiusTemperature = (fahrenheitTemperature - 32) * 5 / 9;
-  return celsiusTemperature;
+  var roundedCelciusTemperature = Math.round(celsiusTemperature * 10) / 10
+  return roundedCelciusTemperature;
 
 };
 
 const convertToFahrenheit = function(celsiusTemperature) {
 
   let fahrenheitTemperature = (celsiusTemperature * 9 / 5) + 32;
+  var roundedFahrenheitTemperature = Math.round(fahrenheitTemperature * 10) / 10;
+  return roundedFahrenheitTemperature;
 
 };
 
